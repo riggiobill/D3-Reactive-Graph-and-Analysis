f1777d852e7e5ad006fd1d518ed9d8b0343e74e7
BillRigg <riggiobill@gmail.com>
2020-11-17 18:20:04 -0500

    Margin info and framework
    Info on SVG margin and declaration info, framework for other tasks in function

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -1,1 +1,46 @@
 // @TODO: YOUR CODE HERE!
+
+
+function makeResponsive() {
+
+
+    // Margin and SVG initialization
+
+    svgWidth = 960;
+    svgHeight = 500;
+    
+    margin = {
+        top: 20,
+        right: 40,
+        bottom: 80,
+        left:100
+    };
+    
+    width = svgWidth - margin.left - margin.right;
+    height = svgHeight - margin.top - margin.bottom;
+
+    // SVG wrapper creation and attribute preparation for svg group
+
+
+    // Use D3.csv and .then to extract data from data.csv
+
+    // Format strings and numbers
+
+    // Create scale for X and Y values
+
+
+
+    // Create axes
+
+    // Append axis to ChartGroup
+
+    // declare and fill out attribute for CircleGroup
+
+    // Make and append labels for chart
+
+
+
+
+}
+
+makeResponsive();
